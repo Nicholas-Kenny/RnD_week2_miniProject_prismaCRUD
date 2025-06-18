3da584fd99bd07248e150efb865b8a802ac0a15b
nicholas hendrawan <nicholas.hendrawan@binus.ac.id>
2025-06-18 11:37:03 +0700

    login

--- src/modules/product/controller/auth.controller.js
+++ src/modules/product/controller/auth.controller.js
@@ -15,8 +15,18 @@ const register = async (req, res) => {
   }
 };
 
-const login = (req, res) => {
-  res.send("Login");
+const login = async (req, res) => {
+  try {
+    const { email, password} = req.body;
+
+    const {user, token} = await authService.login({ email, password });
+  
+    return res.status(200).json(user);
+    
+  } catch (error) {
+    console.log(error);
+    return res.json({ error: "internal Server Error" });
+  }
 };
 
 module.exports = {
--- src/modules/product/service/auth.service.js
+++ src/modules/product/service/auth.service.js
@@ -1,4 +1,6 @@
 const authRepository = require("../repository/auth.repository");
+const bycrypt = require("bcrypt");
+const token = require("../../../helpers/jwt");
 
 const register = async ({ name, email, password, dob }) => {
   const user = await authRepository.user(email);
@@ -7,10 +9,35 @@ const register = async ({ name, email, password, dob }) => {
     throw new Error("User Already Exists");
   }
 
-  const newUser = await authRepository.newUser(name, email, password, dob);
-  return newUser;
+  return await authRepository.newUser(name, email, password, dob);
+};
+
+const login = async ({ email, password }) => {
+  const user = await authRepository.user(email);
+  if (!user) {
+    // return res.json({ error: "User Already Exists" }).status(400);
+    throw new Error("User Does Not Exists");
+  }
+
+  // return await authRepository.newUser(name, email, password, dob);
+  const compare = await bcrypt.compare(password, user.password);
+  if (!compare) {
+    throw new Error("Invalid Credentials");
+  }
+
+  const token = token({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    password: user.password,
+    dob: user.dob,
+    role: user.role
+  });
+
+  return {token, user};
 };
 
 module.exports = {
   register,
+  login,
 };
